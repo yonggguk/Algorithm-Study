2a230ac9fd19c582efa24d2b8c08b309b30568e1
yongguk <yonguk929@gmail.com>
2023-02-22 16:34:33 +0900

    [level 0] Title: 배열 원소의 길이, Time: 2.26 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
-        for(int i = 0; i < strlist.length; i++){
-            answer[i] = strlist[i].length();
-        }
-        return answer;
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
     }
 }
